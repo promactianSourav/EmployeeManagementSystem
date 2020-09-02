774103157498d1269b7685d2e70d1124bb937d21
promactianSourav <sourav@promactinfo.com>
2020-09-02 15:47:18 +0530

    In EF, After bechmarking all the methods in Employee management system.

--- EmployeeManagementSystem/EmployeeManagement/Controllers/DepartmentController.cs
+++ EmployeeManagementSystem/EmployeeManagement/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace EmployeeManagement.Controllers
 {
@@ -25,22 +26,36 @@ namespace EmployeeManagement.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
             ViewBag.departments = _context.Departments.ToList();
-           
+            watch.Stop();
+            Console.WriteLine($"Execution time for select : {watch.ElapsedMilliseconds} ms");
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(Department department)
         {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
             List<Department> listDepartment = new List<Department>();
             listDepartment = _context.Departments.ToList();
             int d = listDepartment.Max(x => x.DeptId);
             department.DeptId = d + 1;
+            for (int i = 1; i < 100; i++)
+            {
+                Department de = new Department();
+                de.DeptId = d + i;
+                de.DepartmentName = "DepEF" + i.ToString();
+                _context.Departments.Add(de);
+            }
 
-
-            _context.Departments.Add(department);
+            //_context.Departments.Add(department);
             _context.SaveChanges();
+            watch.Stop();
+            Console.WriteLine($"Execution time for Insert : {watch.ElapsedMilliseconds} ms");
+
             return RedirectToAction("Index");
         }
 
@@ -57,18 +72,42 @@ namespace EmployeeManagement.Controllers
         [HttpPost]
         public IActionResult Edit(Department department)
         {
-            ViewBag.id = department.DeptId;
-            _context.Departments.Update(department);
-            _context.SaveChanges();
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            //ViewBag.id = department.DeptId;
+            for(int i = 1; i < 100; i++)
+            {
+                Department d = new Department();
+                d.DeptId = i;
+                d.DepartmentName = "DeEFup" + i.ToString();
+                _context.Departments.Update(d);
+                _context.SaveChanges();
+            }
+            //_context.Departments.Update(department);
+            
+            watch.Stop();
+            Console.WriteLine($"Execution time for update : {watch.ElapsedMilliseconds} ms");
+
             return RedirectToAction("Index");
         }
 
         [HttpPost]
         public IActionResult Delete(int Id)
         {
-            Department department = _context.Departments.FirstOrDefault(a => a.DeptId == Id);
-            _context.Departments.Remove(department);
-            _context.SaveChanges();
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            for(int i = 1; i < 100; i++)
+            {
+                Department department = _context.Departments.First();
+                _context.Departments.Remove(department);
+                _context.SaveChanges();
+            }
+            //Department department = _context.Departments.FirstOrDefault(a => a.DeptId == Id);
+            //_context.Departments.Remove(department);
+           
+            watch.Stop();
+            Console.WriteLine($"Execution time for delete : {watch.ElapsedMilliseconds} ms");
+
             return RedirectToAction("Index");
         }
     }
