a89430a264b1b88e97f1c119bd902c1e26088d04
promactianSourav <sourav@promactinfo.com>
2020-09-02 13:14:50 +0530

    In Micro ORM(dapper), After checking the execution time for first time.

--- EmployeeManagementSystem/EmployeeManagement/Controllers/DepartmentController.cs
+++ EmployeeManagementSystem/EmployeeManagement/Controllers/DepartmentController.cs
@@ -27,17 +27,33 @@ namespace EmployeeManagement.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
             ViewBag.departments = departmentSqlMapperAsync.GetAllDepartments().ToList();
+            //watch.Stop();
+            //Console.WriteLine($"Execution time for Select in micro ORM(dapper): {watch.ElapsedMilliseconds}");
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(Department department)
         {
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
             List<Department> listDepartment = new List<Department>();
             listDepartment = departmentSqlMapperAsync.GetAllDepartments().ToList();
-            int d = listDepartment.Max(x => x.DeptId);
-            department.DeptId = d+1;
+            int d1 = listDepartment.Max(x => x.DeptId);
+            department.DeptId = d1+1;
+            //for(int i = 1; i < 110; i++)
+            //{
+            //    Department d = new Department();
+            //    d.DeptId = d1+i;
+            //    d.DepartmentName = "EmployeeMicroORM" + i.ToString();
+            //    departmentSqlMapperAsync.AddDepartment(d);
+
+            //}
+            //watch.Stop();
+            //Console.WriteLine($"Execution time for Insert in micro ORM(dapper): {watch.ElapsedMilliseconds}");
 
             departmentSqlMapperAsync.AddDepartment(department);
             return RedirectToAction("Index");
@@ -67,16 +83,36 @@ namespace EmployeeManagement.Controllers
         [HttpPost]
         public IActionResult Edit(Department department)
         {
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
             ViewBag.id = department.DeptId;
+            //for (int i = 1; i < 215; i++)
+            //{
+            //    Department d = new Department();
+            //    d.DeptId = department.DeptId + i;
+            //    d.DepartmentName = "Depat" + i.ToString();
+            //    departmentSqlMapperAsync.UpdateDepartment(d);
+
+            //}
             departmentSqlMapperAsync.UpdateDepartment(department);
+            //watch.Stop();
+            //Console.WriteLine($"Execution time for Update in micro ORM(dapper): {watch.ElapsedMilliseconds}");
+
             return RedirectToAction("Index");
         }
 
         [HttpPost]
         public IActionResult Delete(int Id)
         {
-
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
+            //for(int i = 1; i < 300; i++)
+            //{
+            //    departmentSqlMapperAsync.DeleteDepartment(i);
+            //}
             departmentSqlMapperAsync.DeleteDepartment(Id);
+            //watch.Stop();
+            //Console.WriteLine($"Execution time for Delete in micro ORM(dapper): {watch.ElapsedMilliseconds}");
 
             return RedirectToAction("Index");
         }
